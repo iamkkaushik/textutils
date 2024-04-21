5d0be91885785cd021a9ca3069793de1620f1aea
kaushikkkkkkk <kaushikkodarapu10@gmail.com>
2024-04-21 13:49:26 +0530

    final

--- src/components/About.js
+++ src/components/About.js
@@ -6,6 +6,7 @@ export default function About(props) {
     //     color: 'black',
     //     backgroundColor: 'white'
     // }) 
+    // hdfs jwt
     let myStyle = {
         color: props.mode ==='dark'?'white':'#042743',
         backgroundColor: props.mode ==='dark'?'rgb(36 74 104)':'white', 
